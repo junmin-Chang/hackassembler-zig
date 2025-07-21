40125873e78d7d8c13e5b9bae2af4fb77c92776a
junmin-Chang <chjm219@kookmin.ac.kr>
2025-07-21 17:48:11 +0900

    basic file i/o logic into main()

--- assembler.zig
+++ assembler.zig
@@ -1,4 +1,23 @@
+const std = @import("std");
 const parser = @import("parser.zig");
 const codegen = @import("codegen.zig");
 
-pub fn main() !void {}
+pub fn main() !void {
+    const cwd = std.fs.cwd();
+
+    // make output_dir to store generated machine code;
+    cwd.makeDir("output") catch |err| switch (err) {
+        error.PathAlreadyExists => {},
+        else => return err,
+    };
+
+    var output_dir = try cwd.openDir("output", .{});
+    defer output_dir.close();
+
+    const file = try output_dir.createFile("output.hack", .{});
+    defer file.close();
+
+    // parsing ... codegen ...
+
+    std.debug.print("Successfully wrote nbytes.\n", .{});
+}

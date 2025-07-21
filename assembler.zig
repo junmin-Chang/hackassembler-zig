ca669350458388f2e5a03b8ffa1a012602f0b713
junmin-Chang <chjm219@kookmin.ac.kr>
2025-07-21 18:23:35 +0900

    pass argument via command line

--- assembler.zig
+++ assembler.zig
@@ -3,7 +3,20 @@ const parser = @import("parser.zig");
 const codegen = @import("codegen.zig");
 
 pub fn main() !void {
+    const args = try std.process.argsAlloc(std.heap.page_allocator);
+    defer std.process.argsFree(std.heap.page_allocator, args);
+
+    if (args.len < 2) {
+        std.debug.print("Usage: ./assembler <filename>\n", .{});
+        return error.MissingArgument;
+    }
+
     const cwd = std.fs.cwd();
+
+    var input_file = try cwd.openFile(args[1], .{});
+    defer input_file.close();
+
+    std.debug.print("filename: {s}, file: {}", .{ args[1], input_file });
 
     // make output_dir to store generated machine code;
     cwd.makeDir("output") catch |err| switch (err) {
@@ -14,8 +27,8 @@ pub fn main() !void {
     var output_dir = try cwd.openDir("output", .{});
     defer output_dir.close();
 
-    const file = try output_dir.createFile("output.hack", .{});
-    defer file.close();
+    const output_file = try output_dir.createFile("output.hack", .{});
+    defer output_file.close();
 
     // parsing ... codegen ...
 
